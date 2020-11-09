730d793121808495eb82c40f082a21688cdb9e0a
Alberto Liguori <a1liguorialberto@gmail.com>
2020-11-10 00:06:07 +0100

    Displays sub-lists on hover

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+$("#mainListItem-1").hover(function(){
+    $("#sub-list-1").toggle()
+})
 
+$("#mainListItem-2").hover(function(){
+    $("#sub-list-2").toggle()
+})
+
+$("#mainListItem-3").hover(function(){
+    $("#sub-list-3").toggle()
+})
+
+$("#mainListItem-4").hover(function(){
+    $("#sub-list-4").toggle()
+})
